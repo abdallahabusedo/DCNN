4829cdada78e4b954ae0a94839382bd2a63de3cb
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-05-10 23:38:42 +0200

    Update rle to write to a file

--- compression/rle.py
+++ compression/rle.py
@@ -1,7 +1,12 @@
 import cv2
 
 
+def readImg(path):
+    return cv2.imread(path, 0)
+
+
 def rle(img):
+    f = open("out.txt", "w")
     ret2, th2 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     th2 = th2/255
     img = th2
@@ -9,15 +14,16 @@ def rle(img):
     for row in img:
         code = 0
         status = 1
+        rowCode = ''
         for col in row:
             if(status == col):
                 code += 1
             else:
-                compressedImg.append(code)
+                f.write(str(code)+'\n')
                 code = 1
                 status = (not status)**1
-        compressedImg.append(code)
-    return compressedImg
+        f.write(str(code)+'\n')
+    f.close()
 
 
 img = readImg("tmp.png")
