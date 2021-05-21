cea53075aa12cb91c943680b552e657ce586617c
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-05-21 22:49:27 +0200

    Update data out

--- data/data.py
+++ data/data.py
@@ -4,10 +4,11 @@ import math
 
 from numpy.core.numeric import False_
 
+dataList = []
 
-def data(path, outPath, isBias=False):
+
+def data(path, isBias=False):
     f = open(path, 'r')
-    fout = open(outPath, 'w')
     Lines = f.readlines()
     count = 0
     # Strips the newline character
@@ -19,19 +20,28 @@ def data(path, outPath, isBias=False):
         for word in numbers:
             number = float(word)
             bitCol = Fxp(number, n_word=16, n_frac=11)
-            fout.write(bitCol.bin()+'\n')
+            dataList.append(bitCol.bin())
     f.close()
+
+
+def outData(outPath):
+    fout = open(outPath, 'w')
+    for number in dataList:
+        fout.write(number+"\n")
     fout.close()
 
 
-data("filtersconv2d_1.txt", "filtersconv2d_1_out.txt")
-data("biasesconv2d_1.txt", "biasesconv2d_1_out.txt", True)
-data("filtersconv2d_2.txt", "filtersconv2d_2_out.txt")
-data("biasesconv2d_2.txt", "biasesconv2d_2_out.txt", True)
-data("filtersconv2d_3.txt", "filtersconv2d_3_out.txt")
-data("biasesconv2d_3.txt", "biasesconv2d_3_out.txt", True)
+data("filtersconv2d_1.txt")
+data("biasesconv2d_1.txt", True)
+data("filtersconv2d_2.txt")
+data("biasesconv2d_2.txt", True)
+data("filtersconv2d_3.txt")
+data("biasesconv2d_3.txt", True)
 
-data("weightsdense_1.txt", "weightsdense_1_out.txt")
-data("biasesdense_1.txt", "biasesdense_1_out.txt", True)
-data("weightsdense_2.txt", "weightsdense_2_out.txt")
-data("biasesdense_2.txt", "biasesdense_2_out.txt", True)
+data("weightsdense_1.txt")
+data("biasesdense_1.txt", True)
+data("weightsdense_2.txt")
+data("biasesdense_2.txt", True)
+
+
+outData("data.txt")
