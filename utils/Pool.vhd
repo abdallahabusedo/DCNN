3841558a6b511897bded0f3aeb170935a20122a5
Nihal Mansour <nihalmansour0599@gmail.com>
2021-05-23 04:32:20 +0200

    updated pool

--- utils/Pool.vhd
+++ utils/Pool.vhd
@@ -1,13 +1,17 @@
 LIBRARY IEEE;
+use ieee.fixed_float_types.all;
+use ieee.fixed_pkg.all;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.all;
 USE IEEE.std_logic_unsigned.all;
 
 package pool_pkg is
-        type bus_array is array(24 DOWNTO 0) of signed(15 DOWNTO 0);
+        type bus_array is array(3 DOWNTO 0) of sfixed (4 downto -11);
 end package;
 
 library ieee;
+use ieee.fixed_float_types.all;
+use ieee.fixed_pkg.all;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 USE IEEE.std_logic_unsigned.all;
@@ -16,36 +20,36 @@ use work.pool_pkg.all;
 ENTITY Pool IS
 	PORT(
 		WINDOW : IN bus_array;
-		AVR : OUT signed(15 DOWNTO 0)
+		AVR : OUT sfixed (4 downto -11)
 		
 	);
 END ENTITY;
 ARCHITECTURE arch_Pool OF Pool IS
+
+signal test : sfixed (4 downto -11);
+
 	BEGIN
 		process(WINDOW)
-			variable sum : signed(16 DOWNTO 0); --16 for overflow
-			variable division : signed(15 DOWNTO 0);
-			variable complement : unsigned(4 DOWNTO 0);
-			variable tempAvr : signed(31 DOWNTO 0);
+			variable sum : sfixed (4 downto -11);
+			variable division : sfixed (4 downto -11);
     			begin
 				sum := (others => '0');
-				division := b"0000000001010001";
-    				for i in 0 to 24 loop
-         				sum := sum + (WINDOW(i)(15) & WINDOW(i));
-					-- handling overflow
-					if sum(16) = '0' and (to_integer(unsigned(sum(15 DOWNTO 11)))>15)then
-						sum(16 DOWNTO 11) := b"001111";
-					elsif sum(16) = '1' then
-						complement := NOT(unsigned(sum(15 DOWNTO 11)));
-						complement := complement + 1;
-						if(to_integer(complement)>16) then 
-							sum(16 DOWNTO 11) := b"110000";
-						end if;
-					end if;  
+				division := to_sfixed (0.25, 4, -11);
+    				for i in 0 to 3 loop
+
+					sum := resize (arg => sum+WINDOW(i), 
+			  				left_index => sum'high ,
+			 				right_index => sum'low ,
+							round_style => fixed_round, 
+							overflow_style => fixed_saturate);  
+
     				end loop;
-				tempAvr := (sum(16)&sum(14 DOWNTO 0))*division;
-				--AVR <= tempAvr(26 DOWNTO 11);
-				AVR <= sum(16)&sum(14 DOWNTO 0);
+		test <= sum;
+		AVR <= resize (arg => sum*division , 
+			  				left_index => AVR'high ,
+			 				right_index => AVR'low ,
+							round_style => fixed_round, 
+							overflow_style => fixed_saturate); 
 		end process;
 	
 		
