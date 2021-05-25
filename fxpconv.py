e3f0ef4fec1705fbf633767ab628a92ac54f284c
HmanA6399 <ebrahim.gomaa.hg@gmail.com>
2021-05-25 23:11:35 +0200

    Edit fixed point script to accept array pf inputs

--- fxpconv.py
+++ fxpconv.py
@@ -3,12 +3,15 @@ from fxpmath import Fxp
 
 
 radix = sys.argv[1]
-xsf = sys.argv[2]
 
+res = ""
 
-x = Fxp(xsf, signed=True, n_word=16, n_frac=11)
+for i in range(2, len(sys.argv)):
+    xsf = sys.argv[i]
+    x = Fxp(xsf, signed=True, n_word=16, n_frac=11)
+    if (radix == 'hex'):
+        res += x.hex()[2:]
+    else:
+        res += x.bin()
 
-if (radix == 'hex'):
-    print(x.hex())
-else:
-    print(x.bin())
+print(res)
